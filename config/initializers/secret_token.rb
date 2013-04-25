4e120b13c3fd84906c289fe3b1345c1d20226ea7
asafiniu <asafiniu@gmail.com>
2013-04-25 13:46:18 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moshemishali::Application.config.secret_token = '150b12b26834bddd848e21b04bab962a139c94fcdf85d9f9e8792aabbc0a2e9978be228c6ed4d5cbaeef6c80b89f024c2ccbea3780487002a2078630d3d1cef3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Moshemishali::Application.config.session_store :cookie_store, key: '_moshemishali_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Moshemishali::Application.config.session_store :active_record_store
