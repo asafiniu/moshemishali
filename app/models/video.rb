a9c57471d785051c85ff60b13f398d7eaa7776fd
asafiniu <asafiniu@gmail.com>
2013-04-28 21:35:54 +0300

    changed DB operator to fit PG

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,6 +2,6 @@ class Video < ActiveRecord::Base
   attr_accessible :description, :title, :url, :typename, :image_url
 
   def self.by_type(typename)
-  	Video.where("typename == ?",typename)
+  	Video.where("typename = ?",typename)
   end
 end
