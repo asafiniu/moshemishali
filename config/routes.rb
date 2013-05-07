e3f431bf9f88215acb700166aa48be8da9d01996
asafiniu <asafiniu@gmail.com>
2013-05-07 22:18:05 +0300

    updated routs.rb

--- config/routes.rb
+++ config/routes.rb
@@ -51,8 +51,8 @@ Moshemishali::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'pages#home'
-
+  root :to => "pages#home"
+  get "/Index.aspx" => "pages#home"
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
